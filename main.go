434b283aa2fd98f7763a1799101bb1ed2ed47f89
Md Yousuf Ali <yousuf3usb@gmail.com>
2023-11-01 20:01:22 +0600

    delete .env and .env.example files

--- main.go
+++ main.go
@@ -8,25 +8,27 @@ import (
 	"os"
 )
 
+var db *sql.DB
+
 func main() {
+	// Capture connection properties.
 	cfg := mysql.Config{
-		User:   os.Getenv("DB_USER"),
-		Passwd: os.Getenv("DB_PASSWORD"),
+		User:   os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net:    "tcp",
 		Addr:   "127.0.0.1:3306",
 		DBName: "recordings",
 	}
-
-	db, err := sql.Open("mysql", cfg.FormatDSN())
-
+	// Get a database handle.
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = db.Ping()
-
-	if err != nil {
-		log.Fatal(err)
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
 	}
-
-	fmt.Println("Connected")
+	fmt.Println("Connected!")
 }
