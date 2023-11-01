dae65c02a41fc3c64e909580fe008d45c8ace247
Md Yousuf Ali <yousuf3usb@gmail.com>
2023-11-01 19:53:53 +0600

    set env variable in .env file and add a .env.example for others to add their config

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"github.com/go-sql-driver/mysql"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Printf("Hello, Data Access!")
+	cfg := mysql.Config{
+		User:   os.Getenv("DB_USER"),
+		Passwd: os.Getenv("DB_PASSWORD"),
+		Addr:   "127.0.0.1:3306",
+		DBName: "recordings",
+	}
+
+	db, err := sql.Open("mysql", cfg.FormatDSN())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected")
 }
